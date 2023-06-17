e8f1255d0b79bea16663f0a1fb32646a41becc49
umut <umut_baykan@hotmail.com>
2023-06-17 17:47:55 +0100

    initial testing configuration for web sockets on react

---
+++ frontend/src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import UseWebSocketTester from '../socket-test/test.js'
+
+function App () {
+  return (
+    <div>
+      <p>Hello world.</p>
+      <UseWebSocketTester />
+    </div>
+  )
+}
+
+export default App
