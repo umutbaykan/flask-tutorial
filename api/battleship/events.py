4939aaec4c2857bd3e47186cd221e82f0021d86a
umut <umut_baykan@hotmail.com>
2023-06-21 15:45:00 +0100

    player ID added to the game state upon joining game on backend

--- api/battleship/events.py
+++ api/battleship/events.py
@@ -4,15 +4,23 @@ from .extensions import socketio
 
 ROOMS = {}
 
-def update_rooms(room_id, configurations):
+def create_new_game_state(room_id, configurations):
     """
-    Updates the global room object. Takes in two variables,
-    ID of the room to update and game configurations.
-    Removes the finished games from the rooms during update.
+    Updates the global room object with a new game state. 
+    Takes in two variables, ID of the room to update and game configurations.
     """
     global ROOMS
     ROOMS[room_id] = configurations
-    return ROOMS
+
+def add_player_to_game(room_id, player_id):
+    """
+    Adds a player to the game based on their user ID stored
+    in session (which is their DB id)
+    """
+    global ROOMS
+    current_players = ROOMS[room_id].get('players', [])
+    current_players.append(player_id)
+    ROOMS[room_id]['players'] = current_players
 
 def list_all_rooms():
     """Returns all the available rooms in the global room object"""
--- api/battleship/room.py
+++ api/battleship/room.py
@@ -1,8 +1,8 @@
-from flask import (Blueprint, make_response, session, jsonify)
+from flask import (Blueprint, make_response, session, jsonify, request)
 
 from .auth import login_required
 from .extensions import socketio
-from .events import update_rooms, list_all_rooms
+from .events import create_new_game_state, list_all_rooms, add_player_to_game
 from .helpers import generate_unique_code
 
 bp = Blueprint('room', __name__, url_prefix='/room')
@@ -11,9 +11,22 @@ bp = Blueprint('room', __name__, url_prefix='/room')
 @login_required
 def create_room():
   session['room'] = generate_unique_code()
-  latest_rooms = update_rooms(session['room'], {"gamestate": "someconfigs"}) 
-  socketio.emit('current_games', latest_rooms)
+  create_new_game_state(session['room'], {"gamestate": "someconfigs"})
+  add_player_to_game(session['room'], session['user_id'])
+
+  # Sends the updated list of games to the lobby
+  socketio.emit('current_games', list_all_rooms())
   return make_response({'room': session['room']}, 200)
+
+@bp.route('join', methods=['POST'])
+@login_required
+def join_room():
+  data = request.json
+  if 'room' in session:
+      del session['room']
+  session['room'] = data['room']
+  add_player_to_game(session['room'], session['user_id'])
+  return make_response({}, 200)
 
 @bp.route('/list', methods=['GET'])
 def list_rooms():
