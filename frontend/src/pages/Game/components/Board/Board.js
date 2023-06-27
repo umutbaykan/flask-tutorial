1335fc8f9283aed46783fb552afbe17c4e9a5abb
umut <umut_baykan@hotmail.com>
2023-06-27 23:31:13 +0100

    cells to have different colours based on ship class

--- frontend/src/pages/Game/components/Board/Board.js
+++ frontend/src/pages/Game/components/Board/Board.js
@@ -1,30 +1,52 @@
-import './Board.css'
-import React, { useContext, useEffect } from "react";
+import "./Board.css";
+import React, { useContext, useEffect, useMemo } from "react";
 import Cell from "../Cell/Cell";
 
 import { GameStateContext } from "../../../../App";
 
 const Board = () => {
-  const [gameState, setGameState] = useContext(GameStateContext);
+  const [gameState, ] = useContext(GameStateContext);
+
+  const parsedBoard = useMemo(() => {
+    if (!gameState) {return {}}
+    const ships = gameState.boards[0].ships;
+    let result = {};
+    ships.forEach((ship) => {
+      const { coordinates, alive } = ship;
+      coordinates.forEach((coordinate, index) => {
+        const status = alive[index] ? {class: ship.name, symbol: ""} : {class: ship.class, symbol: "X"};
+        result[JSON.stringify(coordinate)] = status;
+      });
+    });
+    return result;
+  }, [gameState])
 
   useEffect(() => {
     console.log(gameState);
+    console.log(parsedBoard)
   }, [gameState]);
 
-  if (!gameState) {return null}
+  if (!gameState) {
+    return null;
+  }
 
   return (
     <>
-    <h1>Board</h1>
-    <div className="grid-container">
-      {Array.from({ length: gameState.boards[0].size }, (_, rowIndex) => (
-        <div className="row" key={rowIndex}>
-          {Array.from({ length: gameState.boards[0].size }, (_, colIndex) => (
-            <Cell key={colIndex} coordinates={[rowIndex, colIndex]}/>
-          ))}
-        </div>
-      ))}
-    </div>
+      <h1>Board</h1>
+      <div className="grid-container">
+        {Array.from({ length: gameState.boards[0].size }, (_, rowIndex) => (
+          <div className="row" key={rowIndex}>
+            {Array.from({ length: gameState.boards[0].size }, (_, colIndex) => (
+              <Cell
+                key={colIndex}
+                coordinates={[rowIndex, colIndex]}
+                display={(parsedBoard[JSON.stringify([rowIndex, colIndex])] || {}).symbol }
+                type={(parsedBoard[JSON.stringify([rowIndex, colIndex])] || {}).class }
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </>
   );
 };
--- frontend/src/pages/Game/components/Cell/Cell.js
+++ frontend/src/pages/Game/components/Cell/Cell.js
@@ -1,16 +1,20 @@
 import './Cell.css'
+import propTypes from 'prop-types';
 import React from 'react';
 
-const Cell = ({coordinates}) => {
+const Cell = ({coordinates, display, type}) => {
 
     const handleCellPress = () => {
         console.log(coordinates)
       }
 
     return (
-        <button onClick={handleCellPress} className='cell'>O</button>
+        <button onClick={handleCellPress} className={`cell cell-${type}`}>{display}</button>
         )
 }
+
+Cell.propTypes = { coordinates: propTypes.string, display: propTypes.string, type: propTypes.string}
+
 
 export default Cell
 
