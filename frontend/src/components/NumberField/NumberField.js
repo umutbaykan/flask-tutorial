82a2e13e41c82a707df0c0ef810235ef103d7476
umut <umut_baykan@hotmail.com>
2023-06-29 12:29:09 +0100

    custom game creation configuration form

---
+++ frontend/src/components/NumberField/NumberField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { ErrorMessage, useField } from 'formik';
+
+const NumberField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="number-field">
+      <label htmlFor={field.name}>{label}</label>
+      <input
+        id={field.name} className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
+        {...field} {...props}
+        autoComplete="off"
+        type="number"
+      />
+      <ErrorMessage component="div" name={field.name} className="error" />
+    </div>
+  )
+}
+
+NumberField.propTypes = { label: propTypes.string}
+
+
+export default NumberField;
--- frontend/src/pages/Home/components/CreateGameButton/CreateGameButton.js
+++ frontend/src/pages/Home/components/CreateGameButton/CreateGameButton.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { createRoom } from "../../../../services/room";
 import { useNavigate } from "react-router-dom";
 import { socket } from "../../../../socket";
+
+import GameConfigForm from "../GameConfigForm/GameConfigForm";
+
+import { LoggedInContext } from "../../../../App";
 
 // Remove import from below //
 const gameConfigurations = require("../../../model_objects/game_simple_configs.json");
@@ -9,11 +13,15 @@ const gameConfigurations = require("../../../model_objects/game_simple_configs.j
 
 const CreateGameButton = () => {
   //TODO - forms to pick game configurations and save it as a state to send to server
-  const gameconfigs = gameConfigurations
+  const [loggedIn] = useContext(LoggedInContext);
+  const gameconfigs = gameConfigurations;
   const navigate = useNavigate();
   const [error, setError] = useState("");
 
   const handleRoomCreation = async () => {
+    if (!loggedIn) {
+      navigate("/login");
+    }
     const result = await createRoom(gameconfigs);
     if (result.room) {
       socket.emit("join", result.room);
@@ -25,6 +33,7 @@ const CreateGameButton = () => {
 
   return (
     <>
+      <GameConfigForm handleSubmit={console.log}/>
       <button onClick={handleRoomCreation}>Create Game</button>
       <p>{error}</p>
     </>
